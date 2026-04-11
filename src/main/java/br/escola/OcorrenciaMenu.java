931b6db6f3b2dbeaae90b2ba9ee2147f33076b98
00-italo <italoadr75@gmail.com>
2026-04-10 22:41:10 -0300

    Criado cadastro de ocorrências

--- src/main/java/br/escola/OcorrenciaMenu.java
+++ src/main/java/br/escola/OcorrenciaMenu.java
@@ -1,9 +1,10 @@
 package br.escola;
 
+import java.util.Scanner;
 import java.sql.*;
 
 public class OcorrenciaMenu {
-    public static void ListaOcorrencia()
+    public static void listaOcorrencia()
     {
         String sql = "SELECT tipo, descricao, data_ocorrencia FROM ocorrencia";
 
@@ -24,7 +25,7 @@ public class OcorrenciaMenu {
                 // %s    = texto simples sem formatação especial
                 // %n    = quebra de linha (equivalente ao \n, mas compatível com qualquer SO)
                 System.out.printf("%-30s | %s | %s%n",
-                        rs.getString("tipo"),     // Lê o valor da coluna "nome" da linha atual
+                        rs.getString("tipo"),     // Lê o valor da coluna "tipo" da linha atual
                         rs.getString("descricao"),   // Lê o valor da coluna "codigo"
                         rs.getString("data_ocorrencia"));  // Lê o valor da coluna "status"
             }
@@ -33,6 +34,35 @@ public class OcorrenciaMenu {
             // Captura qualquer erro que tenha ocorrido dentro do try:
             // banco offline, senha errada, query com erro de sintaxe, etc.
             // getMessage() retorna uma descrição legível do problema.
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    public static void registrarOcorrencia(Scanner sc) {
+        // "sc Scanner" recebido do Main para ler os dados que o usuário digitar
+
+        // Esse sc.nextLine() inicial é um "erro" clássica do Scanner!
+        // Quando o usuário digita "4" e pressiona Enter no menu (Main.java),
+        // o sc.nextInt() lê o "4" mas DEIXA o "\n" (Enter) no buffer.
+        // Se não limparmos isso, o próximo sc.nextLine() vai ler esse "\n" vazio
+        // em vez de esperar o usuário digitar o nome. Esse nextLine() "engole" o lixo.
+        sc.nextLine();
+
+        try {
+
+            // Coletamos os dados da nova ocorrência pelo console
+            System.out.print("Tipo: ");
+            String tipo = sc.nextLine();
+
+            System.out.print("Data ocorrência (AAAA-MM-DD): ");
+            String data = sc.next();
+
+            System.out.print("Descrição: ");
+            String desc = sc.nextLine();
+
+            System.out.println("Ocorrencia registrada com sucesso!");
+
+        } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
     }
